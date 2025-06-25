5b96c89b4c1361d8c7e8b9e9f7b086acb298934b
marcislaughter <marci.slaughter@gmail.com>
2025-06-24 22:30:41 -0500

    bugfix

--- bible-cryptogram/src/App.tsx
+++ bible-cryptogram/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Game from './components/Game';
 import Instructions from './components/Instructions';
